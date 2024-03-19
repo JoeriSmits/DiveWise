d5087e66774d870965d6e613be30fb61eb5433fc
Joeri Smits <joeri@webbio.nl>
2024-03-19 20:42:27 +0100

    chore: rename page path

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -4,7 +4,7 @@ const Footer = ({}): JSX.Element => {
     return (
         <div className="flex justify-between items-center container mx-auto py-6 my-16 border-solid border-neutral-500 border-t">
             <Image src="/assets/images/logo.svg" alt="Footer" width={240} height={55} />
-            <span className="text-neutral-600">© 2024 DiveWise Technical Diving</span>
+            <span className="text-neutral-600">© DiveWise Technical Diving</span>
         </div>
     );
 }
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -4,13 +4,15 @@ import Link from 'next/link';
 const NavBar = ({}): JSX.Element => {
     return (
         <div className="flex flex-col md:flex-row justify-between items-center container mx-auto py-3 space-y-6 md:space-y-0 my-4 md:my-0" role="navigation">
-            <Image src="/assets/images/logo.svg" width={250} height={56} alt="Logo image of DiveWise Technical Diving" />
+            <Link href="/">
+                <Image src="/assets/images/logo.svg" width={250} height={56} alt="Logo image of DiveWise Technical Diving" />
+            </Link>
 
             <div className="flex space-x-3 md:space-x-8 text-neutral-800">
                 <Link href="/">Home</Link>
                 <a href="#">Missie</a>
                 <a href="#">Opleidingen</a>
-                <Link href="/over_mij">Over Mij</Link>
+                <Link href="/over-mij">Over Mij</Link>
                 <a href="#">Prijzen</a>
                 <a href="#">Contact</a>
             </div>
