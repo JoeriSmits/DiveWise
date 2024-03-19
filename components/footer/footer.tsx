fdbf5c2376ddaad028b2bd597122db1eaac489ac
Joeri Smits <joeri@webbio.nl>
2024-03-19 19:47:46 +0100

    feat: added footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 const Footer = ({}): JSX.Element => {
     return (
-        <div>
-        <h1>Footer</h1>
+        <div className="flex justify-between items-center container mx-auto py-6 my-16 border-solid border-neutral-500 border-t">
+            <Image src="/assets/images/logo.svg" alt="Footer" width={240} height={55} />
+            <span className="text-neutral-600">© 2024 DiveWise Technical Diving</span>
         </div>
     );
 }
